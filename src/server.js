f10cc847794195a9797270c844106315d526f467
CometLim <bluecomet0716@gmail.com>
2022-07-13 17:11:36 +0900

    Set FrontEnd

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ import express, { application } from "express";
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
-
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log(`listening on http://localhost:3000`);
