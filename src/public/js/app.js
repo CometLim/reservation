3f981f0dd49b0af2105061668d136220c378aa34
CometLim <bluecomet0716@gmail.com>
2022-07-18 15:18:01 +0900

    Realtime chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,45 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`) // socket : 서버로의 연결 
 
-socket.addEventListener("open", () => {
-    console.log("Connect to Browser. :check:");
-});
+function makeMessage(type, payload){
+    const msg = { type,payload } ;
+    return JSON.stringify(msg);
+}
+
+function handleOpen() {
+    console.log("Connect to Browser. O");
+}
+
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
-    console.log("Just got this: ", message.data, " from the server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 });
 
 socket.addEventListener("close", () => {
     console.log("Connected from Server X");
 });
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+}
+
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express, { application } from "express";
+import { type } from "os";
 
 const app = express();
 
@@ -14,13 +15,28 @@ const handleListen = () => console.log(`listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server( { server } );
 
+const sockets = []; // socket들이 보관될 array
+
 wss.on("connection", (socket) => {
+    sockets.push(socket); // socket 마다 array에 진입
+    socket["nickname"] = "Anonymous"
     console.log("Connected to Browser");
     socket.on("close", ()=> console.log("Disconnected From the Browser"));
-    socket.on("message", (message) => {
-        console.log(message.toString());
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+        // if(parsed.type === "new_message"){
+        //     sockets.forEach(aSocket => aSocket.send(parsed.payload));
+        // } else if(parsed.type === "nickname"){
+        //     console.log(parsed.payload);
+        // }
     });
-    socket.send("hello!");
 });
 
 server.listen(3000, handleListen);
+
